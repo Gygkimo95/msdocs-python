451e3ffcd5bf81b99f1f89b4d02228edc4f347aa
mazhenhua <mazhenhua@actionsky.com>
2024-07-26 19:31:53 +0800

    update SQL

--- sql/init.sql
+++ sql/init.sql
@@ -1,3 +1,2 @@
-create database app_test;
 use app_test;
-create table if not exists t1(id BIGINT UNSIGNED auto_increment primary key default 1);
+create table if not exists t1(id BIGINT UNSIGNED auto_increment primary key);
