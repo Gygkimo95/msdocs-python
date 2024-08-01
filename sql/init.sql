96e5a5b73ad2cf2e692292ebbca6fee574abf0a9
mazhenhua <mazhenhua@actionsky.com>
2024-08-01 14:53:54 +0800

    update init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,2 +1,2 @@
 use app_test;
-create table if not exists t5(id BIGINT UNSIGNED auto_increment primary key);
+create table if not exists t5(id BIGINT UNSIGNED auto_increment primary key, xname varchar(200));
